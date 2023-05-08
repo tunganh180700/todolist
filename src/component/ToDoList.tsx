0ca3be06d6a3ca677b99e7ef903c0202f97815b4
Nguyen Tung Anh <anh.nguyen54@ntq-solution.com.vn>
2023-05-08 09:33:58 +0700

    UI 60%

--- src/component/ToDoList.tsx
+++ src/component/ToDoList.tsx
@@ -1,33 +1,78 @@
-import { TextField } from "@mui/material"
-import React from "react"
+import { Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from "@mui/material"
+import { LocalizationProvider } from "@mui/x-date-pickers"
+import { DatePicker } from "@mui/x-date-pickers/DatePicker"
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
+
+import React, { useState } from "react"
 
 const ToDoList = () => {
+  const [status, setStatus] = useState<string>()
+  const handleChange = (e: SelectChangeEvent) => {
+    setStatus(e.target.value as string)
+  }
   return (
-    <div style={{ height: "100vh" }}>
-      <div style={{ display: "flex", backgroundColor: "blue" }}>
-        <div className="form" style={{ display: "flex", flexDirection: "column", rowGap: "20px" }}>
+    <div className="h-[100vh] flex justify-center items-center">
+      <div className="h-[70%] w-[70%] flex gap-x-80 justify-center items-center">
+        <div className="flex flex-col gap-y-[20px] w-[30%] border-2 p-5">
           <div>
-            <TextField id="standard-basic" label="Standard" variant="standard" />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker label="Basic date picker" className="w-full" />
+            </LocalizationProvider>
           </div>
           <div>
-            <TextField id="standard-basic" label="Standard" variant="standard" />
+            <TextField id="outlined-basic" label="Job" variant="outlined" className="w-full" />
           </div>
           <div>
-            <TextField id="standard-basic" label="Standard" variant="standard" />
+            <Button variant="contained" className="w-full">
+              Add
+            </Button>
           </div>
         </div>
-        <div className="table">
-          <table>
-            <tr>
-              <th>Company</th>
-              <th>Contact</th>
-              <th>Country</th>
-            </tr>
-            <tr>
-              <td>Alfreds Futterkiste</td>
-              <td>Maria Anders</td>
-              <td>Germany</td>
-            </tr>
+        <div className="w-[60%]">
+          <table className="w-full text-left text-gray-500 dark:text-gray-400 border">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  -
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Date
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Job
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Status
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="bg-white dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                  -
+                </th>
+                <td className="px-6 py-4">12-12-12</td>
+                <td className="px-6 py-4">Malcolm Lockyer</td>
+                <td className="px-6 py-4">
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label" className="dark:text-white">
+                      Status
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={status}
+                      label="Status"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={10}>Done</MenuItem>
+                      <MenuItem value={20}>To do</MenuItem>
+                      <MenuItem value={30}>Cancel</MenuItem>
+                    </Select>
+                  </FormControl>
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
